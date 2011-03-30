69b9df2c92c075ac61aabba4e60a185792d7eb6b
Dmitry S. Kravtsov <idkravitz@gmail.com>
2011-03-30 18:21:41 +1100

    Small changes in options parsing

--- gar/main.go
+++ gar/main.go
@@ -9,16 +9,40 @@ import (
 var (
     useRLE = flag.Bool("r", false, "use RLE compression")
     useHuffman = flag.Bool("h", false, "use Huffman compression")
+    useCompress = flag.Bool("c", false, "compress")
+    useExtract = flag.Bool("x", false, "extract")
     outputName = flag.String("f", "-", "set output file (default is \"-\" -- stdin)")
 )
 
+const (
+    _ = iota
+    HuffmanCompress
+    RLECompress
+)
+
+type CompressionMethod int
+
+func printUsage() {
+    flag.Usage()
+    os.Exit(0)
+}
+
+func compress (name string, method CompressionMethod) os.Error {
+    file, ok := os.Open(name, os.O_RDONLY, 0777)
+    if ok != nil {
+        return ok
+    }
+    return nil // no error
+}
+
 func main() {
     flag.Parse()
-    if flag.NArg() == 0 {
-        flag.Usage()
-        os.Exit(0)
+    if flag.NArg() == 0 ||
+        (!*useCompress && !*useExtract || *useCompress && *useExtract) ||
+        (!*useHuffman  && !*useRLE     || *useHuffman  && *useRLE) {
+        printUsage()
     }
     for _,name := range flag.Args() {
-        fmt.Printf(name)
+        //fmt.Println(name)
     }
 }
